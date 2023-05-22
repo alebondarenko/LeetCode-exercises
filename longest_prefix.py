5f974aba7e116811bdc54734f3d24d1b0419c646
Alexander Bondarenko <al.bondarenko@yahoo.com>
2023-05-22 22:09:08 +0200

    code

--- longest_prefix.py
+++ longest_prefix.py
@@ -7,7 +7,7 @@ class Solution(object):
 
         result = ""
         i = 1
-        while len(set([w[:i] for w in strs])) == 1:
+        while len(set([w[:i] for w in strs])) == 1: # not efficient: 2 times the same list comprehension
             result = set([w[:i] for w in strs])
             i += 1
         
@@ -16,5 +16,7 @@ class Solution(object):
 
 
 solution = Solution()
+
 print(solution.longestCommonPrefix(["flower","flow","flight"]))
 print(solution.longestCommonPrefix(["dog","racecar","car"]))
+print(solution.longestCommonPrefix(["hyperactive", "hyperspace", "hypertension", "hyperinflation"]))
